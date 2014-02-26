c24b1a3bb4842af69aac3a11d9a3b3c06d561109
Clifford <clif7786@vandals.uidaho.edu>
2014-02-26 11:57:53 -0800

    grrr netbeans

--- src/temp/scrolling_map_demo/client/ScrollDemo.java
+++ src/temp/scrolling_map_demo/client/ScrollDemo.java
@@ -1,8 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
 
 public class ScrollDemo implements MouseListener, KeyListener {
