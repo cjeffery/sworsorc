7a0639b6c001e6b1c48efc00792d59877e9f0eec
drage <drage@crunchbang.(none)>
2014-02-27 15:59:41 -0800

    allows the mouse to move units

---
+++ src/temp/drag_unit/MouseMoveUnit.java
@@ -0,0 +1,13 @@
+/* MouseMoveUnit.java
+* 
+*  Allows the mouse to move units.
+*  Left click and hold on unit to select unit and move.
+*  Release left button to release unit.
+*/
+
+public class MouseMoveUnit.java{
+
+
+
+
+}
