b5a8afe1a09e51d1f63edc6cfeb7e35ad8dd1f00
John Goettsche <goet0969@vandals.uidaho.edu>
2014-03-03 17:59:24 -0800

    dialog changes
    dialog changes

--- src/ChatClient/src/chatclient/ChatClient.java
+++ src/ChatClient/src/chatclient/ChatClient.java
@@ -14,6 +14,8 @@ public class ChatClient {
     private Socket socket = null;
     private DataInputStream console = null;
     private DataOutputStream streamOut = null;
+    private static String ipAddress;
+    private static String username;
     
     public ChatClient(String serverName, int serverPort) throws IOException{
         System.out.println("Connecting! Please Wait!");
@@ -24,7 +26,7 @@ public class ChatClient {
         }catch(UnknownHostException e){
             System.out.println("Uknown host!");
         }
-        String username = "Blarg";
+        //String username = "Blarg";
         String line = "";
         try{
             streamOut.writeUTF(username);
@@ -59,20 +61,25 @@ public class ChatClient {
     public static void main(String[] args) throws IOException{
         //JFrame frame;
         Object[] possibilities = null;
-        String ip = (String)JOptionPane.showInputDialog(
+        ipAddress = (String)JOptionPane.showInputDialog(
                     null,
                     "IP Address of server: ",
                     "IP Address",
                     JOptionPane.CANCEL_OPTION,
                     null,
                     possibilities, "127.0.0.1");
+        username = (String)JOptionPane.showInputDialog(
+                null,
+                "Username",
+                "Username",
+                JOptionPane.CANCEL_OPTION);
         
         ChatClient client = null;
         if(args.length != 2){
             System.out.println("Usage: ChatClient host port");
             System.out.println("Attempting defualt connection!");
             //client = new ChatClient("76.178.139.129", 25565);
-            client = new ChatClient(ip, 25565);
+            client = new ChatClient(ipAddress, 25565);
         }else{
             client = new ChatClient(args[0], Integer.parseInt(args[1]));
         }
