4b8d49167b754a1627c16ce1f57e11478b6f026b
Keith Drew <keithd@vandals.uidaho.edu>
2014-03-06 10:34:40 -0800

    add movement calculator

---
+++ src/client/MovementCalculator/src/MovementCalculator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author keith
+ */
+import java.util.*;
+
+public class MovementCalculator {
+    public static void main( String[] args )
+    {
+        ArrayList myArrList = new ArrayList();
+        
+        for( int i = 0; i < 3; i++ )
+        {
+            myArrList.add(i);
+        }
+        
+        for( int i = 0; i < 3; i++ )
+            System.out.println( myArrList );
+    }    
+
+    
+//This method must use a MoveableUnit object.     
+public static void MovementCalc(int hexID, int moveAllowance, ArrayList hexList)
+{
+    ArrayList validHexes = new ArrayList();
+    
+    
+}
+}
