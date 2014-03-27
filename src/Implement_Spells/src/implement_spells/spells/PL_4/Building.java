49ed60f3084b9dfe54b62fca9f669deb1e3fc16f
simo2703 <simo2703@vandals.uidaho.edu>
2014-03-26 20:11:50 -0700

    Update Implement spells
    added initial functionality for casting spells

--- src/Implement_Spells/src/implement_spells/spells/PL_4/Building.java
+++ src/Implement_Spells/src/implement_spells/spells/PL_4/Building.java
@@ -6,10 +6,72 @@
 
 package implement_spells.spells.PL_4;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  *
- * @author 张涛
+ * @author Tao Zhang & Cameron Simon
  */
-public class Building {
+public final class Building {
+    JFrame frame;
+    
+    public Building(){
+        prepareGUI();
+    }
+    
+    public void prepareGUI(){
+        frame = new JFrame("Building");
+        frame.setSize(100,100);
+        frame.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing( WindowEvent e )
+            {  //System.exit(0); 
+
+            }
+        });
+        
+        JLabel notice = new JLabel("This is Building");
+        
+        frame.add(notice);
+        frame.setVisible(true);
+    }   
+    
+    public void getTarget(){
+        // this function is used to get the target to cast spell
+    }
+    
+    public boolean checkLimits(){
+        boolean limit = false;
+        
+        //if( fit all the limits ){
+          //  limit = true;
+        //}
+        
+        return limit; 
+    }
+    
+    public void performSpellEffects(){
+        // this function is used to perform the spell effects
+        // like cost mana, or the real effects described in rules
+        if(checkLimits() == true){
+            // perform
+            
+            // what I am thinking about performing some data effects
+            // is that we can make a tmp data file that stores all the
+            // char or unit info, 
+            // then we can just go into that file and change the data
+            // then we read the file again for refresh the game data
+            
+            
+        }else{
+            // show warning that it desn't fit all the limitations
+        }
+        
+        
+        
+    }
     
 }
--- src/Implement_Spells/src/implement_spells/spells/PL_7/Wizard_Wheel.java
+++ src/Implement_Spells/src/implement_spells/spells/PL_7/Wizard_Wheel.java
@@ -6,10 +6,74 @@
 
 package implement_spells.spells.PL_7;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  *
- * @author 张涛
+ * @author Tao Zhang & Cameron Simon
  */
-public class Wizard_Wheel {
+public final class Wizard_Wheel {
+    JFrame frame;
+    
+    public Wizard_Wheel(){
+        prepareGUI();
+    }
+    
+    public void prepareGUI(){
+        frame = new JFrame("Wizard_Wheel");
+        frame.setSize(100,100);
+        frame.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing( WindowEvent e )
+            {  //System.exit(0); 
+
+            }
+        });
+        
+        JLabel notice = new JLabel("This is Wizard_Wheel");
+        
+        frame.add(notice);
+        frame.setVisible(true);
+    }   
+    
+    public void getTarget(){
+        // this function is used to get the target to cast spell
+    }
+    
+    public boolean checkLimits(){
+        boolean limit = false;
+        
+        //if( fit all the limits ){
+          //  limit = true;
+        //}
+        
+        return limit; 
+    }
+    
+    public void performSpellEffects(){
+        // this function is used to perform the spell effects
+        // like cost mana, or the real effects described in rules
+        if(checkLimits() == true){
+            // perform
+            
+            // what I am thinking about performing some data effects
+            // is that we can make a tmp data file that stores all the
+            // char or unit info, 
+            // then we can just go into that file and change the data
+            // then we read the file again for refresh the game data
+            
+            
+        }else{
+            // show warning that it desn't fit all the limitations
+        }
+        
+        
+        
+    }
     
 }
+
+
