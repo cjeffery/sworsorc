60d65e398cf0097ecb643095ce2e3c995ff8e31c
Xihark <flak4202@vandals.uidaho.edu>
2014-04-05 15:44:35 -0700

    Sun Image now cycles
    Need to change which images it cycles through (using cleaned improved
    images)

--- src/client/SolarDisplay/SolarDisplay.java
+++ src/client/SolarDisplay/SolarDisplay.java
@@ -27,8 +27,8 @@ public class SolarDisplay extends JFrame implements ActionListener{
 */
     
         JLabel Title, SolarDay, RedSun, BlueSun, SunImageLabel;
-        ImageIcon SunImage;
         JButton AddDay;
+        ImageIcon SunImageIcon, BlueImageIcon, RedImageIcon;
         
         int SolarDayNum = 1;
         int RedSunVal = 1;
@@ -54,11 +54,16 @@ public class SolarDisplay extends JFrame implements ActionListener{
         AddDay.setActionCommand("AddDay");
         AddDay.addActionListener(this);
         
-        ImageIcon SunImageIcon;
         try {
             File SunImage = new File("resources/images/YellowSun.png");
+            File BlueImage = new File("resources/images/BlueSun.png");
+            File RedImage = new File("resources/images/RedSun.png");
             BufferedImage SunImg_buff = ImageIO.read(SunImage);
+            BufferedImage BlueImg_buff = ImageIO.read(BlueImage);
+            BufferedImage RedImg_buff = ImageIO.read(RedImage);
             SunImageIcon = new ImageIcon(SunImg_buff);
+            BlueImageIcon = new ImageIcon(BlueImg_buff);
+            RedImageIcon = new ImageIcon(RedImg_buff);
             SunImageLabel = new JLabel(SunImageIcon);
         }
         catch(IOException e){
@@ -74,8 +79,8 @@ public class SolarDisplay extends JFrame implements ActionListener{
         add(SunImageLabel);
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setBounds(0, 0, 250, 150);
-        pack();
+        setBounds(0, 0, 175, 290);
+//        pack();
         setLocationRelativeTo(null);
         setVisible(true);
 }
@@ -117,112 +122,136 @@ public class SolarDisplay extends JFrame implements ActionListener{
                     if (RedSunVal > 2 && RedSunVal < 6){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 2 && BlueSunVal < 6){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                 case 3: case 4:
                     if (RedSunVal > 3 && RedSunVal < 7){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 3 && BlueSunVal < 7){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 5: case 6:
                     if (RedSunVal > 4 && RedSunVal < 8){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 4 && BlueSunVal < 8){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 7: case 8: case 9:
                     if (RedSunVal > 5 && RedSunVal < 9){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 5 && BlueSunVal < 9){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 10: case 11:
                     if (RedSunVal > 6 && RedSunVal < 10){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 6 && BlueSunVal < 10){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 12: case 13:
                     if (RedSunVal > 7 && RedSunVal < 11){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 7 && BlueSunVal < 11){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 14: case 15:
                     if (RedSunVal > 8 && RedSunVal < 12){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 8 && BlueSunVal < 12){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 16: case 17:
                     if (RedSunVal > 9 && RedSunVal < 13){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 9 && BlueSunVal < 13){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 18: case 19: case 20:
@@ -230,15 +259,18 @@ public class SolarDisplay extends JFrame implements ActionListener{
                             || RedSunVal == 1){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal == 11 || BlueSunVal == 12
                             || BlueSunVal == 1){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 21: case 22:
@@ -246,43 +278,52 @@ public class SolarDisplay extends JFrame implements ActionListener{
                             || RedSunVal == 2){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal == 12 || BlueSunVal == 1
                             || BlueSunVal == 2){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 23: case 24:
                     if (RedSunVal > 0 && RedSunVal < 4){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 0 && BlueSunVal < 4){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
                     case 25: case 26:
                     if (RedSunVal > 1 && RedSunVal < 5){
                         RedSun.setText("Red Sun State: Ascension");
                         BlueSun.setText("Blue Sun State: Descension");
+                        SunImageLabel.setIcon(RedImageIcon);
                     }
                     else if (BlueSunVal > 1 && BlueSunVal < 5){
                         RedSun.setText("Red Sun State: Descension");
                         BlueSun.setText("Blue Sun State: Ascension");
+                        SunImageLabel.setIcon(BlueImageIcon);
                     }
                     else{
                         RedSun.setText("Red Sun State: Equilibrium");
                         BlueSun.setText("Blue Sun State: Equilibrium");
+                        SunImageLabel.setIcon(SunImageIcon);
                     }
                 break;
             }
