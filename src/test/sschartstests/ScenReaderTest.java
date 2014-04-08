e38420c8de4f706fc2b003d6286e886aee1b91bf
tjasz <nukorausa@hotmail.com>
2014-04-08 09:11:36 -0700

    ScenReaderTest draft
    I'm starting to make some JUnit test for the scenario reader - this is a
    template I pushed up in a few rushed minutes - I will get back to it
    tonight.

---
+++ src/test/sschartstests/ScenReaderTest.java
@@ -0,0 +1,32 @@
+/*
+ * All source code is the work of Clinton Jeffery's Spring 2014 Software Engineering 
+ * class at the University of Idaho consisting of the following members:
+ * Brown, Clifford, Drage, Drew, Flake, Fuhrman, Goes, Goetsche, Higley,
+ * Jaszkowiak, Klingenberg, Pearhill, Sheppard, Simon, Wang, Westrope, Zhang
+ */
+
+package sschartstests;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import junit.framework.TestCase;
+
+/**
+ * A test case for the src/utilities/sscharts/ScenarioConfigurationReader.java
+ * class. It runs a few simple JUnit tests to determine if the scenario reader 
+ * behaves as expected.
+ * 
+ * @author Tyler
+ */
+public class ScenReaderTest extends TestCase {
+    public ScenReaderTest(String testName) {
+        super(testName);
+    }
+    
+    /* // this will become a test one day soon
+    public void test01() {
+         
+        assertFalse(test);
+    }*/
+    
+}
