8c9004dcbac3ca289cc13b326ffa4b78db701a29
johngoettsche <goet0969@vandals.uidaho.edu>
2014-04-14 19:47:41 -0700

    Merge branch 'master' of https://github.com/cjeffery/sworsorc
    Conflicts:
    	src/server/systemServer/NetworkClient.java

--- src/server/systemServer/Conductor.java
+++ src/server/systemServer/Conductor.java
@@ -7,6 +7,8 @@
 
 package systemServer;
 
+import Units.MoveableUnit;
+import Units.UnitPool;
 import java.util.List;
 
 /**
@@ -35,7 +37,20 @@ public class Conductor { // Should this be a static singleton?
      * @param tag The message tag
      */
     public void processMessage( List<String> message, String tag ) {
-        
+        if (message.get(0).equals(MessageUtils.UPDATE_UNIT)){
+            UnitPool pool = UnitPool.getInstance();
+            MoveableUnit unit = pool.getUnit(message.get(1));
+            String location = message.get(2);
+            pool.addMove(unit, location);
+        } else if (message.get(0).equals(MessageUtils.ADD_UNIT)){
+            UnitPool pool = UnitPool.getInstance();
+            MoveableUnit unit = pool.getUnit(message.get(1));
+            unit.setRace(message.get(2));
+            unit.setUnitType(message.get(3));            
+            String location = message.get(4);
+            //needs player ID
+            //pool.addUnit(port, unit);
+        }
     }
     
     // For each major part of the game, put a different method that calls those object's methods
