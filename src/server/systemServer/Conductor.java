5bca1207a016b34514bd66f69ae1f84bce3f7ba5
Christopher Goes <goes.chris@gmail.com>
2014-04-15 02:16:59 -0700

    Github is throwing a fit. Looks like I'm going back to doing a bunch of little commits. Is it annoying you? Go yell at the creators of Git, not me.

--- src/server/systemServer/Conductor.java
+++ src/server/systemServer/Conductor.java
@@ -10,14 +10,44 @@ package systemServer;
 import Units.MoveableUnit;
 import Units.UnitPool;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Receives messages passed from NetworkClient and executes the necessary methods
- * @author Christopher Goes
+ * <p>
+ * 
+ * Big picture ("comforting lie"):
+ * 
+ * When another user does something important, an appropriate function in this class magically runs. 
+ * 
+ * For example, someone else moved a unit, and so the unitMoved(...) function is called
+ * here, letting you update your copy of the map and other stuff. 
+ *              
+ * <p>
+ * 
+ * More detail:  
+ * 
+ * NetworkClient has a thread that sits around waiting for messages to arrive from the server.
+ * When something shows up, the thread checks what type of message it is.
+ * Based on the type of message, the thread will automatically call one 
+ * of the functions in this class.
+ * 
+ * <p>
+ * 
+ * Other potential traps:
+ * 
+ * Some of the messages we send over the network are just networking related, and
+ * some of the functions in this class might actually be called after a "back and forth"
+ * with the server. This is handled internally by NetworkClient. So there's not
+ * always nice "one-to-one" relationship between messages send by NetworkClient
+ * and functions received here.
+ * 
+ * @author Christopher Goes...and friends...
  */
 public class Conductor { // Should this be a static singleton?
     
     // Put singletons and other objects here, so conductor can access their methods
+    //Can we do "register" methods? We have access to singletons everywhere already
     
     /**
      * Constructor
@@ -56,7 +86,122 @@ public class Conductor { // Should this be a static singleton?
     // For each major part of the game, put a different method that calls those object's methods
     // The method is passed the message recieved, which is a list of strings List<String>
     
+    /**
+     * Called at the start of the user's Player-Turn.
+     * <p>
+     * The server is informing you that it is now your turn.
+     * This function is NOT called when some other user begins their player-turn. 
+     * Don't confuse Player-Turn with Game-Turn!
+     * 
+     * @see #otherPlayerBeganTurn(java.lang.String)
+     */
+    public void beginUserPlayerTurn(){
+        //call movement phase...
+        //call attack phase...
+    }
     
+    /**
+     * Called when some other user has started their game turn.
+     * <p>
+     * 
+     * The server is letting you know that some other player (who
+     * is not YOU), has started their turn. 
+     * 
+     * @see #beginUserPlayerTurn() 
+     */
+    public void otherPlayerBeganTurn(String otherUsername){
+        //Announcement?
+    }
+    
+    
+    /**
+     * Called at the start of the manna regeneration inter-phase.
+     * 
+     */
+    public void beginMannaRegenerationPhase(){
+        
+    }
+    
+    /**
+     * Called when all users have been assigned a nation.
+     * 
+     * <p>
+     * 
+     * At the start of a game, each player will be assigned a "nation".
+     * 
+     * A "nation" is something like "Dark Lord's Forces". By looking at the
+     * scenario file, this gives information about what troops you get, 
+     * what provinces, etc.
+     * 
+     * All players get this message at the same time. TODO on actual setup handling...
+     * 
+     * @param playerRoles A map from usernames to nations
+     * @param userNation For convenience, this is the user's nation.
+     */
+    public void nationsAssigned(Map<String, String> playerNation, String userNation){
+        
+    }
+    
+    
+    /**
+     * Called at the start of each Game-Turn
+     * <p>
+     * All users in the game will execute this function at the start o
+     * each new Game-Turn (before any inter-phase is executed).
+     * 
+     * Note that a Game-Turn is NOT the same as a Player-Turn. This function
+     * does not imply that it's a particular user's turn.
+     * 
+     * @param turnNumber The current turn number (the first turn is turn 1)
+     * @param totalTurns The total number of turns in the scenario
+     */
+    public void beginNewGameTurn(int turnNumber, int totalTurns){
+        
+    }
+    
+    
+    /**
+     * Called after the Player-Order Determination Inter-Phase has finished
+     * <p>
+     * 
+     * The server is informing you of the turn-orders for the next game-turn.
+     * The player with turn-order 1 will move first, and so on.
+     * 
+     * This function is NOT a signal to start doing turn stuff, even if you
+     * see that you'll be moving first.
+     * 
+     * @param playerOrders A map from usernames to turn orders
+     * @param yourOrder For convenience, this is your player order.
+     * 
+     * @see #beginUserPlayerTurn()
+    */
+    public void newPlayerOrdersAssigned(Map<String, Integer> playerOrders, int yourOrder){
+        
+    }
+    
+    
+    /**
+     * Called after the last Game-Turn has concluded.
+     * <p>
+     * 
+     * Someday, this might have parameters saying who won or lost. 
+     */
+    public void gameFinished(){
+        
+    }
+    
+    
+    /**
+     * This stub is a placeholder for any random events we end up supporting.
+     * Not sure how many separate methods or handlers we'll need.
+     * 
+     * @param eventCode Some sort of identifier for the event
+     */
+    public void randomEventOccurence(String eventCode){
+        
+    }
+    
+     
     
 }
 
