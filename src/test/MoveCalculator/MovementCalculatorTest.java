27d8d21bfb40c7bf9be0f6899fb25336748417e9
KeisterBox <KeisterBox@KeisterBox-PC>
2014-04-26 14:07:00 -0700

    Unit test up and running: MovementCalculatorTest - ensures that the wrapper and get valid moves functions are working properly and provide the same results. Also had to edit some stuff in movementCalculator - some variable declarations and more specifically, adding the wrapper function for getValidMoves and a logical error edit that caused allowance_cache to accept movement onto vortices where validMoves array list wouldn't not have added a vortex hex to the list.

--- src/test/MoveCalculator/MovementCalculatorTest.java
+++ src/test/MoveCalculator/MovementCalculatorTest.java
@@ -10,6 +10,8 @@ import sshexmap.MainMap;
 import Units.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import org.testng.annotations.Test;
 import sshexmap.MapHex;
 
@@ -28,33 +30,34 @@ public class MovementCalculatorTest extends TestCase {
         UnitPool pool = UnitPool.getInstance();
         HashMap<MapHex, Double> moves;
         ArrayList<MapHex> validMoves = new ArrayList<>();
-        ArrayList<MapHex> fromHashMapMoves = new ArrayList<>();
+        ArrayList<MapHex> illegalMoves = new ArrayList<>();
         // Initialize test unit and add to unitpool
         ArmyUnit unit = new LightSword();
         unit.setRace(Race.Human);
         pool.addUnit(playerID, unit, hexID);
-        
-        
-        
+                
         // As of 4/27 I know getValidMoves returns the correct hexes. I'm using 
         // the method as a baseline for the wrapper test.
         MovementCalculator.getValidMoves(unit, map.GetHex(hexID), 
                 unit.getMovement(), validMoves);
         // Get the hashmap with moves:costs for comparison
         moves = MovementCalculator.movementWrapper( unit, map.GetHex( hexID ) );
-        // Compare the keys.
+  
+       
         
-        System.out.println("***************HASHMAP****************");
-        System.out.println(moves);
-        System.out.println("***************ARRAYLIST****************");
-        System.out.println(validMoves);
-        
-        moves.keySet().stream().forEach((keyHex) -> {
-            fromHashMapMoves.add(keyHex);
-        });
+        // print to see hexes/costs
+        /*
+        moves.keySet().stream().forEach((key) -> {
+            String HexId = key.GetID();
+            System.out.println("ID, cost");
+            System.out.println(HexId + ", " + moves.get(key));
+        });*/
+        // Check that sizes are equal - same number of valid moves from both
+        //  sources.
+        System.out.println("Size 1 and 2: " + validMoves.size() + " " + moves.size());
 
         assertEquals("Check the size of both lists is equal", validMoves.size(),
-                fromHashMapMoves.size());
+                moves.size());
     }
     
 }
