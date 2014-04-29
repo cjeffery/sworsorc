e9a78aa8f7f8a0fb50d6549ceee15cb1c94f2180
Shaung <Shaung@192.168.0.101>
2014-04-29 03:30:20 -0700

    Combat like vandals, Combat phase works: selects 2 units and press 'a' to launch combat

--- src/client/mainswordsorcery/HUDController.java
+++ src/client/mainswordsorcery/HUDController.java
@@ -35,6 +35,12 @@ import sshexmap.MapView;
 import systemServer.NetworkClient;
 
 import static java.lang.Integer.parseInt;
+import org.controlsfx.control.Notifications;
+import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialogs;
+import static sscharts.ArmyCombatResultsTable.PrepareAttackResults;
+import ssterrain.TTSwamp;
+import ssterrain.TerrainType;
 
 public class HUDController {
     @FXML private TabPane UnitsPane;
@@ -702,8 +708,56 @@ public class HUDController {
      * @author Jay Drage
      */
     public void StartCombat(){
+        
         System.out.println("StartCombat()");
-        //Place combat code here
+
+        System.out.println(target_unit.getRace());
+        System.out.println(target_unit.getNation());
+        
+        MoveableUnit selected_combat_unit = new MoveableUnit();
+        MoveableUnit target_combat_unit = new MoveableUnit();
+        selected_combat_unit = selected_unit;
+        target_combat_unit = target_unit;
+        selected_combat_unit.setUnitType(UnitType.ArmyUnit);
+        target_combat_unit.setUnitType(UnitType.ArmyUnit);
+        
+        ArrayList<ArmyUnit> attackers = new ArrayList<>();
+        ArrayList<ArmyUnit> defenders = new ArrayList<>();
+ 
+        attackers.add((ArmyUnit) selected_combat_unit);
+        attackers.add((ArmyUnit) selected_combat_unit);
+        defenders.add((ArmyUnit) target_combat_unit);
+        defenders.add((ArmyUnit) target_combat_unit);
+        MapHex hex1 = new MapHex();
+        TerrainType tt1 = null;
+        tt1 = new TTSwamp();
+        hex1.setTerrainType(tt1);  
+        target_combat_unit.getLocation();
+        ArrayList result = new ArrayList();
+        result = PrepareAttackResults(attackers, defenders, hex1);
+        int[] index = (int[])result.get(0);
+        int atk = (int)result.get(1);
+        int def = (int)result.get(2);
+        int after_def = (int)result.get(3);
+        double ratio = (double)result.get(4);
+
+        Notifications.create()
+              .title("Details of Combat")
+              .text("attacker: " + atk + "\ndefender: " + def + "\nAfter Def: " + after_def)
+              .showWarning();
+ 
+        
+        Action result_of_combat = Dialogs.create()
+          .title("Combat Comfirmation")
+          .message( "Are you sure to enforce the combat?")
+          .showConfirm();
+
+        
+        Notifications.create()
+        .title("Result of Combat")
+        .text("Attackets: Stay.\nDefenders: Retreat " + index[1] + " Units.")
+        .showWarning();
+
     }
     /**
      * used to start spells
