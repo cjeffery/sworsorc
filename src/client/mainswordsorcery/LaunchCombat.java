4ac0d75bba8e4b9110c2ef20193ba0a2fc9a8d24
Shaung <Shaung@192.168.0.101>
2014-05-02 01:34:45 -0700

    Friendly Units now can add into combat, defender terrain bonus works(fixed) now.

--- src/client/mainswordsorcery/LaunchCombat.java
+++ src/client/mainswordsorcery/LaunchCombat.java
@@ -13,15 +13,18 @@ import java.util.ArrayList;
 import java.util.List;
 import org.controlsfx.control.Notifications;
 import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialog;
 import org.controlsfx.dialog.Dialogs;
 import static sscharts.ArmyCombatResultsTable.PrepareAttackResults;
 import sshexmap.MapHex;
+import sshexmap.MapView;
 import ssterrain.TTSwamp;
 import ssterrain.TerrainType;
 
 /**
  *
  * @author Shaung
+ * 
  */
 public class LaunchCombat {
     
@@ -51,33 +54,68 @@ public class LaunchCombat {
         if (target_stack.size() > 1)
         defenders.add((ArmyUnit) target_combat_unit1);
         
+        
+        // Set up Defender's TerrainType
+        MapHex Defender_Terrain = new MapHex();
+        MapView temp = MapView.getMapView();
+        Defender_Terrain = (MapHex)temp.GetHexMap().GetHex(selected_combat_unit.getLocation());
+        
+        // Add Friendly Unit into Attackers List
+        ArrayList<MapHex> FriendlyCombatList = new ArrayList<>();
+        FriendlyCombatList = getFriendlyList(Defender_Terrain);
+        System.out.println("Numbers of friendly units: " + FriendlyCombatList.size());
+        
+        for (int i = 0; i < FriendlyCombatList.size(); i++) {
+            Action Add_into_Attacker_List = Dialogs.create()
+            .title("Adding Friendly Units into Combat")
+            .message( "Do you Want to Add" + FriendlyCombatList.get(i).getUnits().get(0).getID() + " to the attackers list?")
+            .showConfirm();
+ 
+        
+            if (Add_into_Attacker_List == Dialog.Actions.YES) {
+                Notifications.create()
+                .title("Add into List")
+                .text("Unit added into Attackers List")           
+                .showWarning();
+                attackers.add((ArmyUnit)FriendlyCombatList.get(i).getUnits().get(0));
+            } 
+            else {
+                Notifications.create()
+                .title("Discard from List")
+                .text("Unit discard from the Attackers List")           
+                .showWarning();
+            }
+            
+        
+        }
+
+        
+        // Details of the Attackers and Defenders List
         Notifications.create()
         .title("Details of List")
         .text("Attacker 1 Race: " + selected_combat_unit.getRace() + "\nAttacker 2 Race: " + selected_combat_unit1.getRace() +
-              "\nAttacker 1 ID: " + selected_combat_unit.getID() + "\nAttacker 2 ID: " + selected_combat_unit1.getID()
-        
-        
+              "\nAttacker 1 ID: " + selected_combat_unit.getID() + "\nAttacker 2 ID: " + selected_combat_unit1.getID()        
              )
         .showWarning();
         
         
-        // Set up Defender's TerrainType
-        MapHex hex1 = new MapHex();
-        TerrainType tt1 = null;
-        tt1 = new TTSwamp();
-        hex1.setTerrainType(tt1);
         
-        //target_combat_unit.getLocation();
+        
+
         
         // Get Combat Result Here
         ArrayList result = new ArrayList();
-        result = PrepareAttackResults(attackers, defenders, hex1);
+        result = PrepareAttackResults(attackers, defenders, Defender_Terrain);
         int[] index = (int[])result.get(0);
         int atk = (int)result.get(1);
         int def = (int)result.get(2);
         int after_def = (int)result.get(3);
         double ratio = (double)result.get(4);
         
+        
+
+        
+
         // Pop up Combat Result
 
  
@@ -87,10 +125,16 @@ public class LaunchCombat {
           .message( "Are you sure to enforce this combat?" +
                     "\nAttackers Strength: " + atk + 
                     "\nDefenders Strength: " + def + 
+                    "\nDefenders' Terrain Type is: " + Defender_Terrain.getTerrainType() +
                     "\nAfter Terrain Bonus: " + after_def
                   )
           .showConfirm();
 
+        if (result_of_combat == Dialog.Actions.YES) {
+        
+        
+        
+        
         if (index[0] == 0 && index[1] == 0) {
             Notifications.create()
             .title("Result of Combat: Draw")
@@ -137,5 +181,35 @@ public class LaunchCombat {
             }
         }
         
+        }
+        else {
+            // Do nothing
+        }
+        
     }
+    /**
+     * 
+     * @param currHex
+     * @author Shaung & Kidth
+     * @return ArrayList<MapHex>
+     * 
+     */
+    public static ArrayList<MapHex> getFriendlyList( MapHex currHex )
+    {
+        ArrayList<MapHex> neighbors = new ArrayList<>();
+        //For each hex edge, 0-5, get the neighboring hex, if it's valid
+        for( int i = 0; i < 6; i++ )
+        {
+            // Make sure the neighbor exists
+            if( currHex.getNeighbor( i ) != null && currHex.getNeighbor(i).getUnits() != null)
+                neighbors.add( currHex.getNeighbor(i));
+        }
+        
+        
+        return neighbors;
+    }
+    
+    
+    
+    
 }
